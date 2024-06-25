08b10050b056c85a0ab5c19cd3eef2331ab15f16
fox4kun <omer@elsawy.sd>
2024-06-25 23:41:41 +0200

    Update telegram_downloader_bot.py

--- telegram_downloader_bot.py
+++ telegram_downloader_bot.py
@@ -1,56 +1,6 @@
-#!/usr/bin/python
-# -*- coding: utf8 -*-
-
-# telegram-downloader-bot: File downloader from Telegram
-# Author: Alfonso E.M. <alfonso@el-magnifico.org>
-# Requires:
-#  telegram_bot https://pypi.python.org/pypi/python-telegram-bot/
-#  a bot token (ask @BotFather)
-#
-# Version: 1.8
-
-from __future__ import print_function
-
-import time
-from os import path, getenv, makedirs
-import requests
-
-from multiprocessing import Process, Manager
-from telegram import Bot, ReplyKeyboardMarkup
-from telegram.error import BadRequest 
-
-# CONFIGURATION
-TELEGRAM_TIMEOUT=int(getenv("TELEGRAM_TIMEOUT", "50"))
-TELEGRAM_BOT_TOKEN=getenv("TELEGRAM_BOT_TOKEN", "YOUR TOKEN HERE") # <-- Configure here
-TELEGRAM_CHAT_ID=getenv("TELEGRAM_CHAT_ID", "YOUR CHAT ID HERE") # <-- Configure here
-TELEGRAM_REFRESH_SECONDS=int(getenv("TELEGRAM_REFRESH_SECONDS", "1"))
-DOWNLOADS_FOLDER=getenv("DOWNLOADS_FOLDER", "/downloads")
-makedirs(DOWNLOADS_FOLDER, exist_ok=True)
-# END CONFIGURATION
-
-# DOWNLOAD INDEPENDENT PROCESS
-def downloader(filenames, urls):
-    while True:
-        try:
-            filename = filenames.pop(0)
-            url = urls.pop(0)
-        except IndexError:
-            time.sleep(5)
-            continue
-
-        if filename == "QUIT":
-            break
-
-        if filename:
-            print("Downloading: " + filename + ' from ' + url)
-            r = requests.get(url, stream=True)
-            with open(path.join(DOWNLOADS_FOLDER, filename), 'wb') as f:
-                for chunk in r.iter_content(chunk_size=1024): 
-                    if chunk: 
-                        f.write(chunk)
-            print("Download completed")
-
-if __name__ == '__main__':
+def main():
+    # الكود الخاص بتشغيل البوت
+    # تأكد من نقل الكود الموجود داخل الـ __main__
     bot = Bot(TELEGRAM_BOT_TOKEN)
 
     bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="Downloader ready!")
@@ -59,7 +9,7 @@ if __name__ == '__main__':
     manager = Manager()
     filenames = manager.list()
     urls = manager.list()
-    download_process = Process(target=downloader, args=(filenames, urls,))
+    download_process = Process(target=downloader, args=(filenames,urls,))
     download_process.daemon = True
     download_process.start()
 
@@ -80,12 +30,13 @@ if __name__ == '__main__':
                 user_command = update.message.text
             except AttributeError:
                 user_command = None
+                pass
 
             if user_command and user_command.lower() == "quit":
                 filenames.append("QUIT")
                 download_process.join()
                 user_quit = True
-                bot.get_updates(offset=update_id, timeout=TELEGRAM_TIMEOUT)  # Mark as read
+                telegram_updates = bot.get_updates(offset=update_id, timeout=TELEGRAM_TIMEOUT)
                 break
 
             elif user_command == "?":
@@ -94,7 +45,7 @@ if __name__ == '__main__':
             try:
                 newfile = update.message.document
                 bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="Downloading file %s (%i bytes)" % (newfile.file_name, newfile.file_size))
-                tfile = bot.get_file(newfile.file_id)
+                tfile = bot.getFile(newfile.file_id)
                 filenames.append(newfile.file_name)
                 urls.append(tfile.file_path)
             except BadRequest:
@@ -105,11 +56,11 @@ if __name__ == '__main__':
 
             try:
                 photos = update.message.photo
-                if photos:
+                if len(photos) > 0:
                     photo = photos[-1]
                     name = "{}-{}x{}.jpg".format(update.update_id, photo.width, photo.height)
                     bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="Downloading photo %s (%i bytes)" % (name, photo.file_size))
-                    tfile = bot.get_file(photo.file_id)
+                    tfile = bot.getFile(photo.file_id)
                     filenames.append(name)
                     urls.append(tfile.file_path)
             except AttributeError:
@@ -119,7 +70,7 @@ if __name__ == '__main__':
                 video = update.message.video
                 name = "{}-{}x{}.mp4".format(update.update_id, video.width, video.height)
                 bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="Downloading video %s (%i bytes)" % (name, video.file_size))
-                tfile = bot.get_file(video.file_id)
+                tfile = bot.getFile(video.file_id)
                 filenames.append(name)
                 urls.append(tfile.file_path)
             except BadRequest:
@@ -132,7 +83,7 @@ if __name__ == '__main__':
                 audio = update.message.audio
                 name = u"{}-{}-{}.mp3".format(audio.title, audio.performer, update.update_id)
                 bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="Downloading audio %s (%i bytes)" % (name, audio.file_size))
-                tfile = bot.get_file(audio.file_id)
+                tfile = bot.getFile(audio.file_id)
                 filenames.append(name)
                 urls.append(tfile.file_path)
             except BadRequest:
@@ -145,10 +96,13 @@ if __name__ == '__main__':
                 voice = update.message.voice
                 name = "{}.ogg".format(update.update_id)
                 bot.send_message(chat_id=TELEGRAM_CHAT_ID, text="Downloading voice %s (%i bytes)" % (name, voice.file_size))
-                tfile = bot.get_file(voice.file_id)
+                tfile = bot.getFile(voice.file_id)
                 filenames.append(name)
                 urls.append(tfile.file_path)
             except AttributeError:
                 pass
 
         time.sleep(TELEGRAM_REFRESH_SECONDS)
+
+if __name__ == '__main__':
+    main()
